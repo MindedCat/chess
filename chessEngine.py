cdc62248c017d60c4e27f467fa9de2b951c2a79e
MindedCat <mindedtomcat@gmail.com>
2025-09-20 10:36:20 +0600

    version 0.1

--- chessEngine.py
+++ chessEngine.py
@@ -3,3 +3,20 @@
 -> checks for valid moves
 -> keeps move log
 """
+import numpy as np
+
+class GameState:
+    def __init__(self):
+        self.board = np.array([
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],       #8x8 board
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],       #1st character color = b,w
+            ["--", "--", "--", "--", "--", "--", "--", "--"],       #2nd character piece = P,R,N,B,Q,K
+            ["--", "--", "--", "--", "--", "--", "--", "--"],       #empty space = --
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ])
+
+        self.whiteToMove = True
+        self.moveLog = []
--- chessMain.py
+++ chessMain.py
@@ -3,3 +3,65 @@ main driver file
 -> takes user input
 -> displays current GameState object
 """
+
+import pygame as pg
+import chessEngine
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8
+SQ_SIZE = WIDTH // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+#loads all images once
+def loadImages():
+    pieces = ["wP","wR", "wN", "wB", "wQ", "wK","bP", "bR", "bN", "bQ", "bK", "bB"]
+
+    for piece in pieces:
+        IMAGES[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+#graphics for the current game state
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+# draw squares on the board
+def drawBoard(screen):
+    colors = [pg.Color("white"), pg.Color("gray")]
+
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            color = colors[((row+col)%2)]
+            pg.draw.rect(screen, color, pg.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+# draw pieces on those square
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            piece = board[row][col]
+
+            if piece != "--":
+                screen.blit(IMAGES[piece], pg.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+    screen.fill(pg.Color("white"))
+
+    gs = chessEngine.GameState()
+    loadImages()
+
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        pg.display.flip()
+
+
+if __name__ == "__main__":
+    main()
